e12b543a617be96a23a2b32ff0f40914e0347d36
zsgsdesign <zsgsdesign@gmail.com>
2021-08-23 16:30:23 +0800

    update fitting NOJ 0.16.0

--- updateTheme.php
+++ updateTheme.php
@@ -1,8 +1,8 @@
 <?php
     $themeConfig=[];
-    $themeList = json_decode(file_get_contents("themes/themelist.json"), false);
+    $themeList = json_decode(file_get_contents("new_themes/themelist.json"), false);
     foreach($themeList as $themeCode => $themeFile) {
-        $themeConfig[$themeCode]=json_decode(file_get_contents("themes/$themeFile.json"), false);
+        $themeConfig[$themeCode]=json_decode(file_get_contents("new_themes/$themeFile.json"), false);
     }
     file_put_contents("theme.json", json_encode($themeConfig, JSON_PRETTY_PRINT));
     file_put_contents("theme.min.json", json_encode($themeConfig));
